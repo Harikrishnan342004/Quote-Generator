d1cb26e518cf2bd95d912b6165856e02048d0ed5
Hari <harikrishnan53828@gmail.com>
2024-07-13 12:01:21 +0530

    hari

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const generateQuote = () => {
         .then(data => {
             const randomIndex = Math.floor(Math.random() * data.length);
             const randomQuote = data[randomIndex];
-            document.getElementById("quote").innerText = randomQuote.text;
-            document.getElementById("author").innerText = randomQuote.author ? randomQuote.author : "Unknown";
+            document.getElementById("quote").innerText = `"${randomQuote.text}"`;
+            document.getElementById("author").innerText = ` - ${randomQuote.author}`;
         })
         .catch(error => console.error('Error fetching the quotes:', error));
 };
